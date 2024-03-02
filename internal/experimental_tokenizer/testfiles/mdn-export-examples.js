40c30d022565032111b6dd22066e3086c10f0001
stilt0n <mattias.leino@arts.cornish.edu>
2024-03-01 23:38:08 -0500

    add test cases from mdn import and export docs

---
+++ internal/experimental_tokenizer/testfiles/mdn-export-examples.js
@@ -0,0 +1,39 @@
+// This comes from the MDN docs on exports and should be useful
+// in helping test all export cases
+// https://developer.mozilla.org/en-US/docs/web/javascript/reference/statements/export
+
+// Exporting declarations
+// TODO: Support these. These cases are trickier, if it weren't for
+// ASI then we could use semi colons to make handling these easy, but
+// since it exists that's not going to be a possibility.
+// export let name1, name2/*, … */; // also var
+// export const name1 = 1, name2 = 2/*, … */; // also var, let
+export function functionName() { /* … */ }
+export class ClassName { /* … */ }
+export function* generatorFunctionName() { /* … */ }
+export const { name1, name2: bar } = o;
+export const [ name1, name2 ] = array;
+
+// Export list
+export { name1, /* …, */ nameN };
+export { variable1 as name1, variable2 as name2, /* …, */ nameN };
+// TODO: consider supporting string aliases for exports.
+// export { variable1 as "string name" };
+export { name1 as default /*, … */ };
+
+// Default exports
+export default expression;
+export default function functionName() { /* … */ }
+export default class ClassName { /* … */ }
+export default function* generatorFunctionName() { /* … */ }
+export default function () { /* … */ }
+export default class { /* … */ }
+export default function* () { /* … */ }
+
+// Aggregating modules
+export * from "module-name";
+export * as name1 from "module-name";
+export { name1, /* …, */ nameN } from "module-name";
+export { import1 as name1, import2 as name2, /* …, */ nameN } from "module-name";
+export { default, /* …, */ } from "module-name";
+export { default as name1 } from "module-name";
