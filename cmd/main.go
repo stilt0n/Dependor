800a77bf97ba5b83f235eb858afc1f0f2f2f2cef
stilt0n <mattias.leino@arts.cornish.edu>
2024-05-26 18:51:43 -0400

    add cli flags

--- cmd/main.go
+++ cmd/main.go
@@ -1,20 +1,39 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 
 	"github.com/stilt0n/dependor"
 )
 
 func main() {
+	var writeFlag = flag.Bool("write", false, "Write output to dependor-output.json file")
+	var prettyPrintFlag = flag.Bool("pretty", false, "Pretty print output to stdout")
+	flag.Parse()
+
 	graphParser := dependor.NewSync(".")
 	graph, err := graphParser.ParseGraph()
 	if err != nil {
 		fmt.Printf("Got an error. Error: %s", err)
 		return
 	}
-	printGraph(graph)
-	graph.WriteToJSONFile()
+
+	if *writeFlag {
+		graph.WriteToJSONFile()
+		return
+	}
+
+	if *prettyPrintFlag {
+		printGraph(graph)
+		return
+	}
+
+	jsonOutput, err := graph.WriteToJSONString()
+	if err != nil {
+		fmt.Printf("An error occurred when stringifying the output:\n%s", err)
+	}
+	fmt.Println(jsonOutput)
 }
 
 func printGraph(graph dependor.DependencyGraph) {
